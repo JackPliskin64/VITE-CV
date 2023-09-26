143f08f38b01b65685e7b00a280306c9ab4500c7
JackPliskin64 <jackpliskin64@outlook.es>
2023-09-26 15:02:51 +0200

    Se cambian las secciones correctamente

--- main.js
+++ main.js
@@ -15,3 +15,54 @@ function filterSkills() {
     }
   }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Obtén los elementos de navegación
+  var skillsLink = document.querySelector('a[href="#skills"]');
+  var contactLink = document.querySelector('a[href="#contact"]');
+
+  // Obtén las secciones de contenido
+  var skillsContent = document.getElementById('skills-content');
+  var contactContent = document.getElementById('contact-content');
+
+  // Maneja el clic en "Skills"
+  skillsLink.addEventListener('click', function (e) {
+    e.preventDefault();
+    // Oculta todas las secciones de contenido
+    hideAllContent();
+    // Muestra la sección de Skills
+    skillsContent.classList.remove('hidden');
+  });
+
+  // Maneja el clic en "Contact"
+  contactLink.addEventListener('click', function (e) {
+    e.preventDefault();
+    // Oculta todas las secciones de contenido
+    hideAllContent();
+    // Muestra la sección de Contact
+    contactContent.classList.remove('hidden');
+  });
+  // Obtén la etiqueta "Introduction"
+  var introductionLink = document.querySelector('a[href="#introduction"]');
+  // Obtén la sección "Introduction"
+  var introductionContent = document.getElementById('introduction-content');
+
+  // Maneja el clic en "Introduction"
+  introductionLink.addEventListener('click', function (e) {
+    e.preventDefault();
+    // Oculta todas las secciones de contenido
+    hideAllContent();
+    // Muestra la sección "Introduction"
+    introductionContent.classList.remove('hidden');
+  });
+
+
+
+  // Función para ocultar todas las secciones de contenido
+  function hideAllContent() {
+    skillsContent.classList.add('hidden');
+    contactContent.classList.add('hidden');
+    introductionContent.classList.add('hidden');
+  }
+
+});
