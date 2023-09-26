91ea7bf1145787cea237005988a1ab98e90d590f
JackPliskin64 <jackpliskin64@outlook.es>
2023-09-26 15:14:37 +0200

    Quitar el error de transición. Falta añadir transición suave

--- main.js
+++ main.js
@@ -17,52 +17,34 @@ function filterSkills() {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  // Obtén los elementos de navegación
   var skillsLink = document.querySelector('a[href="#skills"]');
   var contactLink = document.querySelector('a[href="#contact"]');
-
-  // Obtén las secciones de contenido
+  var introductionLink = document.querySelector('a[href="#introduction"]'); // Nueva línea
   var skillsContent = document.getElementById('skills-content');
   var contactContent = document.getElementById('contact-content');
+  var introductionContent = document.getElementById('introduction-content'); // Nueva línea
 
-  // Maneja el clic en "Skills"
   skillsLink.addEventListener('click', function (e) {
     e.preventDefault();
-    // Oculta todas las secciones de contenido
     hideAllContent();
-    // Muestra la sección de Skills
     skillsContent.classList.remove('hidden');
   });
 
-  // Maneja el clic en "Contact"
   contactLink.addEventListener('click', function (e) {
     e.preventDefault();
-    // Oculta todas las secciones de contenido
     hideAllContent();
-    // Muestra la sección de Contact
     contactContent.classList.remove('hidden');
   });
-  // Obtén la etiqueta "Introduction"
-  var introductionLink = document.querySelector('a[href="#introduction"]');
-  // Obtén la sección "Introduction"
-  var introductionContent = document.getElementById('introduction-content');
 
-  // Maneja el clic en "Introduction"
-  introductionLink.addEventListener('click', function (e) {
+  introductionLink.addEventListener('click', function (e) { // Nueva función para "Introduction"
     e.preventDefault();
-    // Oculta todas las secciones de contenido
     hideAllContent();
-    // Muestra la sección "Introduction"
     introductionContent.classList.remove('hidden');
   });
 
-
-
-  // Función para ocultar todas las secciones de contenido
   function hideAllContent() {
     skillsContent.classList.add('hidden');
     contactContent.classList.add('hidden');
     introductionContent.classList.add('hidden');
   }
-
 });
