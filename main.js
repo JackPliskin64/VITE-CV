e955b72ecfb82fe6705af5c46ea22c750ab6d69f
JackPliskin64 <jackpliskin64@outlook.es>
2023-09-27 23:42:14 +0200

    Small fixes, fully added projects category

--- main.js
+++ main.js
@@ -19,9 +19,11 @@ document.addEventListener('DOMContentLoaded', function () {
   var skillsLink = document.querySelector('a[href="#skills"]')
   var contactLink = document.querySelector('a[href="#contact"]')
   var introductionLink = document.querySelector('a[href="#introduction"]')
+  var projectsLink = document.querySelector('a[href="#projects"]')
   var skillsContent = document.getElementById('skills-content')
   var contactContent = document.getElementById('contact-content')
   var introductionContent = document.getElementById('introduction-content')
+  var projectsContent = document.getElementById('projects-content')
   var footer = document.querySelector('footer')
 
   skillsLink.addEventListener('click', function (e) {
@@ -54,13 +56,25 @@ document.addEventListener('DOMContentLoaded', function () {
     }, 100)
   })
 
+  projectsLink.addEventListener('click', function (e) {
+    e.preventDefault()
+    hideAllContent()
+    projectsContent.classList.remove('hidden')
+    setTimeout(function () {
+      projectsContent.style.opacity = 1
+      footer.style.opacity = 1 // Mostrar el footer
+    }, 100)
+  })
+
   function hideAllContent() {
     skillsContent.classList.add('hidden')
     contactContent.classList.add('hidden')
     introductionContent.classList.add('hidden')
+    projectsContent.classList.add('hidden')
     skillsContent.style.opacity = 0
     contactContent.style.opacity = 0
     introductionContent.style.opacity = 0
+    projectsContent.style.opacity = 0
     footer.style.opacity = 0 // Ocultar el footer
   }
 })
