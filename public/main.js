d39154201d754e04b67cc0693eac8a7fe48a9d8b
JackPliskin64 <jackpliskin64@outlook.es>
2023-09-28 13:47:53 +0200

    FIngers crossed

---
+++ public/main.js
@@ -0,0 +1,80 @@
+function filterSkills() {
+  var input = document.getElementById('filter')
+  var filter = input.value.toUpperCase()
+  var ul = document.getElementById('skill-elements')
+  var li = ul.getElementsByTagName('li')
+
+  for (var i = 0; i < li.length; i++) {
+    var textP = li[i].getElementsByTagName('p')[0]
+    var textValue = textP.textContent || textP.innerText
+    if (textValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = 'block'
+    } else {
+      li[i].style.display = 'none'
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  var skillsLink = document.querySelector('a[href="#skills"]')
+  var contactLink = document.querySelector('a[href="#contact"]')
+  var introductionLink = document.querySelector('a[href="#introduction"]')
+  var projectsLink = document.querySelector('a[href="#projects"]')
+  var skillsContent = document.getElementById('skills-content')
+  var contactContent = document.getElementById('contact-content')
+  var introductionContent = document.getElementById('introduction-content')
+  var projectsContent = document.getElementById('projects-content')
+  var footer = document.querySelector('footer')
+
+  skillsLink.addEventListener('click', function (e) {
+    e.preventDefault()
+    hideAllContent()
+    skillsContent.classList.remove('hidden')
+    setTimeout(function () {
+      skillsContent.style.opacity = 1
+      footer.style.opacity = 1 // Mostrar el footer
+    }, 100)
+  })
+
+  contactLink.addEventListener('click', function (e) {
+    e.preventDefault()
+    hideAllContent()
+    contactContent.classList.remove('hidden')
+    setTimeout(function () {
+      contactContent.style.opacity = 1
+      footer.style.opacity = 1 // Mostrar el footer
+    }, 100)
+  })
+
+  introductionLink.addEventListener('click', function (e) {
+    e.preventDefault()
+    hideAllContent()
+    introductionContent.classList.remove('hidden')
+    setTimeout(function () {
+      introductionContent.style.opacity = 1
+      footer.style.opacity = 1 // Mostrar el footer
+    }, 100)
+  })
+
+  projectsLink.addEventListener('click', function (e) {
+    e.preventDefault()
+    hideAllContent()
+    projectsContent.classList.remove('hidden')
+    setTimeout(function () {
+      projectsContent.style.opacity = 1
+      footer.style.opacity = 1 // Mostrar el footer
+    }, 100)
+  })
+
+  function hideAllContent() {
+    skillsContent.classList.add('hidden')
+    contactContent.classList.add('hidden')
+    introductionContent.classList.add('hidden')
+    projectsContent.classList.add('hidden')
+    skillsContent.style.opacity = 0
+    contactContent.style.opacity = 0
+    introductionContent.style.opacity = 0
+    projectsContent.style.opacity = 0
+    footer.style.opacity = 0 // Ocultar el footer
+  }
+})
