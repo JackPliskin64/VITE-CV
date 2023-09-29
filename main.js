8f905e8bc0210f778e600e271249068fa21a09ad
JackPliskin64 <jackpliskin64@outlook.es>
2023-09-29 14:47:45 +0200

    FInal deploy for bootcamp

--- main.js
+++ main.js
@@ -74,19 +74,3 @@ document.addEventListener('DOMContentLoaded', function () {
     footer.style.opacity = 0 // Ocultar el footer
   }
 })
-
-function filterSkills() {
-  var input = document.getElementById('filter')
-  var filter = input.value.toUpperCase()
-  var ul = document.getElementById('skill-elements')
-  var li = ul.getElementsByTagName('li')
-  for (var i = 0; i < li.length; i++) {
-    var textP = li[i].getElementsByTagName('p')[0]
-    var textValue = textP.textContent || textP.innerText
-    if (textValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = 'block'
-    } else {
-      li[i].style.display = 'none'
-    }
-  }
-}
